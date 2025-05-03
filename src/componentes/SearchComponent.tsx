92cdf193a24c2415fc7156b1cd075097eeb86490
markusluan <markus23_luan@hotmail.com>
2025-05-03 04:23:02 -0300

    Preparando para pesquisar por usuário

--- src/componentes/SearchComponent.tsx
+++ src/componentes/SearchComponent.tsx
@@ -3,10 +3,12 @@ import { useRef, useState } from "react"
 import './SearchComponent.css';
 
 export function SearchComponent () {
-    const [ search, setSearch ] = useState();
+    const [ search, setSearch ] = useState('');
     const [ results, setResults ] = useState([134, 321321, 132121]);
     const [ position, setPosition ] = useState( {top: 0, left: 0, width: 0} );
     const [ isShowResults, setIsShowResults ] = useState(false);
+    
+    const debounceRef = useRef<NodeJS.Timeout | null>(null);
 
     const updatePosition = (e) => {
         const rect = e.target.getBoundingClientRect();
@@ -19,12 +21,57 @@ export function SearchComponent () {
         console.log(position);
     };
 
+    const pesquisar = () => {
+        setResults([Math.random() * 200]);
+        setIsShowResults(true);
+    }
+
+    const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setSearch(value);
+
+        if (debounceRef.current) {
+            clearTimeout(debounceRef.current);
+        }
+
+        debounceRef.current = setTimeout(() => {
+            if (value.trim().length > 0) pesquisar();
+            else {
+                setResults([]);
+                setIsShowResults(false);
+            }
+        }, 500);
+    }
+
+    const onSearchKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.keyCode == 27) {
+            if (debounceRef.current) {
+                clearTimeout(debounceRef.current);
+            }
+
+            setIsShowResults(false);
+        }
+    };
+
+    const onResultSelect = (res: any) => {
+        if (debounceRef.current) {
+            clearTimeout(debounceRef.current);
+        }
+
+        console.log(res);
+
+        setIsShowResults(false);
+    };
+
     return (
         <>
             <input
                 className='component-search form-control'
                 type="text"
                 placeholder='Pesquisar'
+                value={search}
+                onChange={onSearchChange}
+                onKeyDown={onSearchKeyDown}
                 onFocus={(e) => updatePosition(e)} />
             { isShowResults &&
                 <div 
@@ -35,7 +82,10 @@ export function SearchComponent () {
                         width: position.width,
                 }}>
                     {results.map((res, idx) => (
-                    <div key={idx}>{res}</div>
+                        <div
+                            key={idx}
+                            onClick={() => onResultSelect(res)}
+                        >{res}</div>
                     ))}
                 </div>
             }
